a685fa3ead6d3c27b1b048982521116c2012afe2
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-08-03 22:49:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,10 +5,12 @@ class Song
   
   attr_accessor :name
   attr_reader :artist
-
+  
+ extend Memorable::ClassMethods
+ 
   @@songs = []
   
-   extend Memorable::ClassMethods
+  
 
   def initialize
     @@songs << self
